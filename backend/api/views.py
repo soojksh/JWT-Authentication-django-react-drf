2ac18633d6fcfee6722154cfaa99fd4f042ca469
sooj <soojksh@gmail.com>
2024-07-06 06:52:28 +0545

    backend: completion

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 
 User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
